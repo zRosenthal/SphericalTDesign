bc0f7a66cf35548924ff8b3d55c6cd9a4c87b238
Zachary Rosenthal <rosent76@msu.edu>
2016-04-21 12:01:20 -0400

    added mid of G

--- S2DController/s2dcontroller.h
+++ S2DController/s2dcontroller.h
@@ -12,9 +12,11 @@ class S2DController {
 
     private:
         std::array<SphIntvPoint, MATRIX_DIM> pointSet_;
+        myMatrix<cxsc::interval> G;
     public:
         S2DController(std::array<SphIntvPoint, MATRIX_DIM>);
         myMatrix<cxsc::interval> buildYt();
         myMatrix<cxsc::interval> build();
+        std::pair<myMatrix<cxsc::interval>,myMatrix<cxsc::interval>> midG();
 };
 #endif
